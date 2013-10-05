75bfda9c58f90e98dd3e86730afe7b9e32a33873
saeel amar <amar1061@gmail.com>
2013-10-05 16:40:27 +0530

    Tree visualization

--- report.php
+++ report.php
@@ -12,7 +12,8 @@ $(document).ready(function(){
 var reportId = 2;
   $("button").click(function(){
 		//renderLayout(reportId);
-		renderSunChart(reportId);
+		//renderSunChart(reportId);
+		renderTreeChart(reportId);
     // $.ajax({url:"fetchReport.php?reportId="+reportId+"",success:function(result){
 		
 		// if($.trim(result) == 0)
@@ -175,6 +176,54 @@ function arcTween(a) {
 d3.select(self.frameElement).style("height", height + "px");
 
 }
+
+function renderTreeChart(reportId)
+{
+var diameter = 960;
+
+var tree = d3.layout.tree()
+    .size([360, diameter / 2 - 120])
+    .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+
+var diagonal = d3.svg.diagonal.radial()
+    .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", diameter)
+    .attr("height", diameter - 150)
+  .append("g")
+    .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+
+d3.json("fetchReport.php?reportId="+reportId, function(error, root) {
+  var nodes = tree.nodes(root),
+      links = tree.links(nodes);
+
+  var link = svg.selectAll(".link")
+      .data(links)
+    .enter().append("path")
+      .attr("class", "link")
+      .attr("d", diagonal);
+
+  var node = svg.selectAll(".node")
+      .data(nodes)
+    .enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+
+  node.append("circle")
+      .attr("r", 4.5);
+
+  node.append("text")
+      .attr("dy", ".31em")
+      .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+      .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
+      .text(function(d) { return d.name; });
+});
+
+d3.select(self.frameElement).style("height", diameter - 150 + "px");
+
+}
+
 </script>
 </head>
 <body>
@@ -183,7 +232,8 @@ d3.select(self.frameElement).style("height", height + "px");
 
 <div id="div1"><h2></h2></div>
 <button>Get Content</button>
-<style>
+<!-- For the Sun chart-->
+<!-- style>
 
 body {
   font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
@@ -198,6 +248,29 @@ form {
   top: 10px;
 }
 
+</style -->
+
+<!--For the tree chart -->
+
+<style>
+
+.node circle {
+  fill: #fff;
+  stroke: steelblue;
+  stroke-width: 1.5px;
+}
+
+.node {
+  font: 10px sans-serif;
+}
+
+.link {
+  fill: none;
+  stroke: #ccc;
+  stroke-width: 1.5px;
+}
+
 </style>
+
 </body>
 </html>
