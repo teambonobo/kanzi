8821b8293e590e3eca6d3fea5b0296383f93e0da
saeel amar <amar1061@gmail.com>
2013-10-06 06:23:35 +0530

    renaming

--- index.php
+++ index.php
@@ -1,76 +1,143 @@
-<?php 
-if(isset($_GET['login'])){ 
-    if($_GET['login'] == 'failed'){ 
-        define('ERROR', 'true'); 
-    }
-}
-?>
 <!DOCTYPE html>
-<html lang="en" manifest="offline.appcache">
+<html>
 <head>
 <meta charset="utf-8" />
-<title>Login</title>
+<title>Dashboard</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!--[if lt IE 9]>
 <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
+<script src="js/jquery-1.10.1.min.js"> </script>
+<script src="js/d3.v3.min.js"></script>
+<script src="js/charts.js"></script>
+<script src="js/tooltip.js"></script>
+
 <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+<link rel="stylesheet" href="css/tooltip.css" type="text/css">
 <link rel="stylesheet" href="css/bootstrap-responsive.css" type="text/css">
 <link rel="stylesheet" href="css/styles.css" type="text/css">
+<?php  
+include 'DashboardAction.php';
+$publicResult = getPublicReportEntries(); 
+//$privateResult = getPrivateReportEntries();  ?>
 </head>
 
 <body>
-	<header class="header">	
-    	<div class="navbar navbar-fixed-top">
-        	<div class="navbar-inner">
-        		<div class="container-fluid txtcenter">
-                	<a class="brand fnone" href="#"><img src="img/Bonobo_logo.png"/></a>
-                </div>
+	<header class="dashboardheader">
+        <div class="navbar">
+            <div class="navbar-inner">
+                 <div>
+                   <!-- <a class="brand logotxt" href="index.html">BonoboXYZ</a>-->
+                    <form class="form-search">
+                        <div class="input-append">
+                            <input type="text" class="input-medium search-query" placeholder="search...">
+                            <span class="add-on"><i class="icon-search"></i></span>
+                        </div>
+                    </form>
+                    <ul class="nav pull-right">
+                        <li class="dropdown">
+                            <a href="#">
+                                <i class="icon-user"></i>
+                                <span class="caret"></span>
+                            </a>
+                        </li>
+                    </ul>                               l 
+                 </div>
             </div>
         </div>
-   	</header>
-   	<section class="formDiv">
-    	<form name="Login" class="form-horizontal" method="post" action="./auth/auth.php">
-        	<div class="txtcenter">
-            	<div class="control-group <?php if(ERROR == 'true') echo "error"; ?>">
-                        <?php
-                            if(ERROR == 'true')
-                                echo "<span class=\"help-inline\">Invalid User Name/Password</span>";
-                            ?>
-                    <div class="controls">
-                        <div class="input-prepend">
-                            <span class="add-on"><i class="icon-user"></i></span>
-                            <input type="text" placeholder="User Name" name="id">
-                        </div>
-                    </div>
-               </div>
+    </header>
                 
-                <div class="control-group <?php if(ERROR == 'true') echo "error"; ?>" >
-                    <div class="controls">
-                        <div class="input-prepend">
-                            <span class="add-on"><i class="icon-lock"></i></span>
-                            <input type="password" placeholder="Password" name="password">
+	<section>
+    	<div class="row-fluid">
+        	<div id="leftpane" class="span2">
+                <div class="logo"><a class="brand" href="index.html"><img src="img/Bonobo_logo.png"/></a></div>
+            	<ul class="nav nav-list menulinks">
+                	<li class="nav-header">Reports List</li>
+					<?php foreach($publicResult as $k=>$v) { ?>
+						<li><a href="javascript:void(0);" onclick="renderSunChart(<?php echo  $v['id']; ?>);" class="reportsTab"><?php echo $v['report_name']; ?></a></li>
+					<?php } ?>
+              	</ul>
+         	</div>
+            
+            <div id="dashContainer" class="span10">
+            	
+                <div class="container-fluid">
+                	<div class="row-fluid">
+                    	<div class="span12">
+                        	<div class="contentHeader">
+                                <div class="pull-left"></div>
+                                <div class="pull-right">
+                                	<ul class="nav nav-pills">
+                                        <li class="active">
+											<a href="javascript:getReport(1)">Sun Burst</a>
+                                        </li>
+                                        <li><a href="javascript:getReport(2)">Dendrograph</a></li>
+                                        
+                                	</ul>
+                                </div>
+                            </div>
+                            <div class="visualisation" id="visualisation">
+                            	
+                            </div>
                         </div>
                     </div>
                 </div>
                 
-                <div class="control-group">
-                  	<div class="controls">
-                    	<button type="submit" class="btn btn-primary btn-custom">Sign in</button>
-                    </div>
-                </div>
             </div>
-        </form>
-    </section>
+        </div>
+    </section>	
     
-    <footer class="footer">
-    	<div class="txtcenter">
-    		<div>Designed and Powered by <a href="#">Bonobo</a></div>
-        	<div>&copy;2013 Bonobo. All rights reserved. </div>
-        </div>
-    </footer>
-<script src="http://code.jquery.com/jquery.js"></script>
+    
+<script src="js/jquery-1.10.1.min.js"></script>
 <script src="js/bootstrap.js"></script>
 <script src="js/placeholder.js"></script>
+<script>
+	$(document).ready(function(){
+		var winHeight= $(window).height()
+		//alert(winHeight);
+		//$('#dashContainer').height(winHeight);
+		$('#dashContainer').css('min-height', winHeight);
+		$('#leftpane').css('min-height', winHeight);
+	});
+</script>
+<style>
+
+circle {
+  fill: rgb(31, 119, 180);
+  fill-opacity: .25;
+  stroke: rgb(31, 119, 180);
+  stroke-width: 1px;
+}
+
+.leaf circle {
+  fill: #ff7f0e;
+  fill-opacity: 1;
+}
+
+text {
+  font: 10px sans-serif;
+}
+
+.node circle {
+  fill: #fff;
+  stroke: steelblue;
+  stroke-width: 1.5px;
+}
+
+.node {
+  font: 10px sans-serif;
+}
+
+.link {
+  fill: none;
+  stroke: #ccc;
+  stroke-width: 1.5px;
+}
+
+text {
+  font: 10px sans-serif;
+}
+
+</style>
 </body>
 </html>
